eb8d828a893b3d871b9d7b9d6a4157ea13fef2e3
jabagdoshvili <jabagdoshvili@gmail.com>
2021-02-11 13:01:42 +0400

    ....

--- main.js
+++ main.js
@@ -1,48 +1,48 @@
 var swiper = new Swiper('.swiper-container', {
-    effect: 'fade',
-    mousewheel: true,
-    // loop: true,
-    autoplay: {
-        delay: 2500,
-        disableOnInteraction: false,
-      },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+  effect: 'fade',
+  mousewheel: true,
+  // loop: true,
+  autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
     },
-  });
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
 
 
-  function startZero(n) {
-    return (n<10) ? '0' + n : 'n';
+function startZero(n) {
+  return (n<10) ? '0' + n : 'n';
 }
 
 var swiperteam = new Swiper('.swiper-container-team', {
-    slidesPerView: 1,
-    spaceBetween: 10,
-    centeredSlides: true,
-    mousewheel: true,
-    initialSlide:2,
-    // init: false,
-    navigation: {
-        nextEl: '.swiper-button-next-team',
-        prevEl: '.swiper-button-prev-team'
+  slidesPerView: 1,
+  spaceBetween: 10,
+  centeredSlides: true,
+  mousewheel: true,
+  initialSlide:2,
+  // init: false,
+  navigation: {
+      nextEl: '.swiper-button-next-team',
+      prevEl: '.swiper-button-prev-team'
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
+      spaceBetween: 20,
     },
-    breakpoints: {
-      640: {
-        slidesPerView: 1,
-        spaceBetween: 20,
-      },
-      768: {
-        slidesPerView: 1,
-        spaceBetween: 40,
-      },
-      1024: {
-        slidesPerView: 3,
-        spaceBetween: 50,
-      },
-    }
-  });
+    768: {
+      slidesPerView: 1,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 50,
+    },
+  }
+});
 
 
 const sum = startZero(swiper.slides.length)
@@ -52,26 +52,39 @@ const teamsum = startZero(swiperteam.slides.length)
 document.querySelector('.team-arrows .sum').innerHTML = teamsum
 
 swiper.on('slideChange', function () {
-        let active = startZero(swiper.activeIndex + 1)
-        document.querySelector('.swiper-container .active').innerHTML = active
+      let active = startZero(swiper.activeIndex + 1)
+      document.querySelector('.swiper-container .active').innerHTML = active
 
-        document.querySelector('.active-line').style.width = `calc(100%/${swiper.slides.length - swiper.activeIndex})`
-  });
+      document.querySelector('.active-line').style.width = `calc(100%/${swiper.slides.length - swiper.activeIndex})`
+});
 
 
-  swiperteam.on('slideChange', function () {
-    let activeteam = startZero(swiperteam.activeIndex + 1)
-    document.querySelector('.team-arrows .active').innerHTML = activeteam
+swiperteam.on('slideChange', function () {
+  let activeteam = startZero(swiperteam.activeIndex + 1)
+  document.querySelector('.team-arrows .active').innerHTML = activeteam
 
-    document.querySelector('.team-active-line').style.width = `calc(100%/${swiperteam.slides.length - swiperteam.activeIndex})`
+  document.querySelector('.team-active-line').style.width = `calc(100%/${swiperteam.slides.length - swiperteam.activeIndex})`
 });
 
 
 
 $(document).ready(function() {
 
-    var teamheight = $('.team-text').height()
+  var teamheight = $('.team-text').height()
 
-    $('.profile').css('height', teamheight)
+  $('.profile').css('height', teamheight)
 
 });
+
+
+
+$('.menu a').click(function() {
+  
+  $('.menu-display').toggleClass('active')
+  
+});
+
+
+
+
+
